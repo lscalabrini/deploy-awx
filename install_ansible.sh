7571255acfecb7fa4226f547aa58cde8cc86b741
root <root@ip-172-31-66-253.ec2.internal>
2021-03-25 22:48:52 +0000

    beta1

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum -y install ansible
+wget https://github.com/ansible/awx/archive/refs/tags/17.1.0.tar.gz
+tar -zxvf 17.1.0.tar.gz
 
-#
-ansible-playbook  localhost prepare-awx.yml -vv
+echo 
+ansible-playbook  prepare-awx.yml -vv
+echo " \n\n\n Comenzando instalación de AWX... \n\n\n"
+sleep 2
+cd /root/deploy-awx/awx-17.1.0/installer
+ansible-playbook -i inventory install.yml  
 
 
