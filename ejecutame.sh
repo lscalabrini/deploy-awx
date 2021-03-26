346ea56d39becffe66a4c7c5f8c50b5ca316c6a5
root <lscala@gmail.com>
2021-03-26 00:54:14 +0000

    rc1

--- ejecutame.sh
+++ ejecutame.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum -y install ansible
-wget https://github.com/ansible/awx/archive/refs/tags/17.1.0.tar.gz
-tar -zxvf 17.1.0.tar.gz
-
-echo 
+echo "Preparando el entorno para instalar AWX" . \n 
+sleep 2
 ansible-playbook  prepare-awx.yml -vv
-echo " \n\n\n Comenzando instalación de AWX... \n\n\n"
+echo  "Comenzando instalación de AWX..."
 sleep 2
 cd /root/deploy-awx/awx-17.1.0/installer
 ansible-playbook -i inventory install.yml  
